d95a961581baa873d74d21dafa12feef77aed09a
jasonc <jpacahela@gmail.com>
2021-10-19 16:33:17 -0500

    initial commit

---
+++ backend/db/migrations/20201030162614-create-user.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true,
+      },
+      profilePicture: {
+        type: Sequelize.TEXT,
+        defaultValue: "https://cdn.discordapp.com/attachments/886336420552269847/900130882671751250/default_user_picture.png"
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Users");
+  },
+};
