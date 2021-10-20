db5ef052c53a219ff0f14c1d4cddf7caaba45c55
jasonc <jpacahela@gmail.com>
2021-10-19 19:09:56 -0500

    testing automatic heroku deployments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,11 @@ import SignupFormPage from './components/SignupFormPage';
 import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from './store/session';
 import Navigation from './components/Navigation';
-import { Modal } from './context/Modal';
+// import { Modal } from './context/Modal';
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
@@ -25,6 +25,9 @@ function App() {
       )} */}
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <h1>Home Page</h1>
+          </Route>
           <Route path="/login" >
             <LoginFormPage />
           </Route>
