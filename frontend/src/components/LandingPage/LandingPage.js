5e910a04622a70bb6fc482b28b79bded8821eae3
jasonc <jpacahela@gmail.com>
2021-10-19 23:40:46 -0500

    finished styling for landing page background

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
-import './LandingPage.css'
 import Navigation from '../Navigation';
+import LandingCloudsSvg from './LandingCloudsSvg';
+import LandingGraphicLeft from './LandingGraphicLeft';
+import LandingGraphicRight from './LandingGraphicRight';
+import './LandingPage.css'
 
 function LandingPage() {
     return ( 
         <div className="landing-container">
             <Navigation isLoaded={true}/>
+            <LandingCloudsSvg className="landing-clouds-svg"/>
+            <LandingGraphicLeft className="landing-graphic-left-svg" />
+            <LandingGraphicRight className="landing-graphic-right-svg"/>
+            <h1 className="landing-header">
+                YOU NEED A LITTLE MORE DISCOURSE IN YOUR LIFE...
+            </h1>
+            <p className="landing-sub-header">...talk with others around the world, and hang out with the people you love!</p>
         </div>
      );
 }
