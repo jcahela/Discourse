4859315bea2c53dcfc0f3232ff6110852890ae66
jasonc <jpacahela@gmail.com>
2021-10-21 11:14:07 -0500

    set up starter modal when clicking new modal button

--- frontend/src/components/NewServerButton/NewServerButton.js
+++ frontend/src/components/NewServerButton/NewServerButton.js
@@ -1,10 +1,21 @@
 import './NewServerButton.css'
+import { Modal } from '../../context/Modal';
+import { useState } from 'react';
 
 function NewServerButton() {
+    const [showServerModal, setShowServerModal] = useState(false)
+    
     return ( 
-        <div className="new-server-button-container">
-            <span className="new-server-button-plus-icon">+</span>
-        </div>
+        <>
+            <div onClick={() => setShowServerModal(true)} className="new-server-button-container">
+                <span className="new-server-button-plus-icon">+</span>
+            </div>
+            { showServerModal && (
+                <Modal onClose={() => setShowServerModal(false)}>
+                    <h1>New Server Modal</h1>
+                </Modal>
+            )}
+        </>
      );
 }
 
