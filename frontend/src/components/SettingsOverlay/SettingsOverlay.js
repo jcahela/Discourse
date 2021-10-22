f67121e808a26d2bd0884e68df6f04e5d3d82b40
jasonc <jpacahela@gmail.com>
2021-10-22 10:15:57 -0500

    started working on settinvs overlay for server edit and delete crud. Verified that it's possible to open the modal inside an opened modal by importing modal to the first modal content component

--- frontend/src/components/SettingsOverlay/SettingsOverlay.js
+++ frontend/src/components/SettingsOverlay/SettingsOverlay.js
@@ -1,13 +1,28 @@
+import { Modal } from '../../context/Modal'
+import { useState } from 'react';
+
 import './SettingsOverlay.css'
 
 function SettingsOverlay({ server, onClose }) {
+    const [showModal, setShowModal] = useState();
+
     return ( 
         <div className="overlay-container">
-            <div className="overlay-options"></div>
+            <div className="overlay-options">
+                <div className="overlay-options-list">
+                    <h1 className="overlay-name">{server.name}</h1>
+                    <div className="overlay-overview-selector">Overview</div>
+                    <div onClick={() => setShowModal(true)} className="overlay-delete-button">Delete Server</div>
+                </div>
+            </div>
             <div className="overlay-info">
-                <h1>Server Name: {server.name}</h1>
                 <div onClick={onClose} className="close-settings">x</div>
             </div>
+            { showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <h1>Test second modal for delete</h1>
+                </Modal>
+            ) }
         </div>
     );
 }
