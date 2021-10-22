da041dcef7888828e657dea6a390d885b54768c3
jasonc <jpacahela@gmail.com>
2021-10-21 21:42:59 -0500

    added loading screen component

---
+++ frontend/src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,11 @@
+import './LoadingScreen.css'
+
+function LoadingScreen() {
+    return ( 
+        <div className="loading-screen-container">
+            <img className="loading-gif" src="https://cdn.discordapp.com/attachments/886336420552269847/900936387031887892/discord-loading-screen.gif" alt="" />
+        </div>
+    );
+}
+
+export default LoadingScreen;
