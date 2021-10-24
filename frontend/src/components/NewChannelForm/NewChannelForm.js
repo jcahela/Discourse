fcb23008813a08759f2546c01f441651b3b8fd2a
jasonc <jpacahela@gmail.com>
2021-10-24 00:13:43 -0500

    added character counter to create channel form

--- frontend/src/components/NewChannelForm/NewChannelForm.js
+++ frontend/src/components/NewChannelForm/NewChannelForm.js
@@ -8,6 +8,7 @@ function NewChannelForm({ server, onClose }) {
     const dispatch = useDispatch();
     const [channelName, setChannelName] = useState('')
     const [newChannelErrors, setNewChannelErrors] = useState([])
+    const [nameCharacterCounter, setNameCharacterCounter] = useState(25 - channelName.length)
 
     const submitAddChannel = async (e) => {
         e.preventDefault();
@@ -25,6 +26,11 @@ function NewChannelForm({ server, onClose }) {
         onClose();
     }
 
+    const updateChannelName = (e) => {
+        setChannelName(e.target.value);
+        setNameCharacterCounter(25 - e.target.value.length)
+    }
+
     const channelNameContainsOnlySpaces = channelName.replace(/\s/g, '').length === 0
 
     return ( 
@@ -36,14 +42,15 @@ function NewChannelForm({ server, onClose }) {
                     type="text" 
                     className="new-channel-name-input" 
                     value={channelName}
-                    onChange={(e) => setChannelName(e.target.value)}
+                    onChange={updateChannelName}
                 />
-                {newChannelErrors.map((error, index) => (
-                        <div key={index} className="channel-error-container">
-                            <p className="channel-error">{error}</p>
-                        </div>
-                ))}
+                <p className={`new-channel-name-character-counter new-channel-character-length-exceeded-${nameCharacterCounter < 0}`}>{nameCharacterCounter}</p>
             </label>
+            {newChannelErrors.map((error, index) => (
+                <div key={index} className="channel-error-container">
+                    <p className="channel-error">{error}</p>
+                </div>
+            ))}
             <div className="new-channel-cancel-submit-container">
                 <span className="new-channel-cancel-button" onClick={onClose}>Cancel</span>
                 <button 
