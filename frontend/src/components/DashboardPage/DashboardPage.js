bec4a85095369ddd64d40b3d73407df1bff2149b
jasonc <jpacahela@gmail.com>
2021-10-25 18:21:54 -0500

    set client side connection to be heroku when in production

--- frontend/src/components/DashboardPage/DashboardPage.js
+++ frontend/src/components/DashboardPage/DashboardPage.js
@@ -17,7 +17,7 @@ import { io } from 'socket.io-client';
 
 let serverUrl;
 if (process.env.NODE_ENV === "production") {
-    serverUrl = 'https://localhost:5000'
+    serverUrl = 'https://discourse-aa.herokuapp.com/'
 } else {
     serverUrl = 'http://localhost:5000'
 }
