e5c28beb7abfbe8a2fd28b47033405874c1c1a83
jasonc <jpacahela@gmail.com>
2021-10-27 10:28:40 -0500

    finished adding about links

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import Navigation from '../Navigation';
 import LandingCloudsSvg from './LandingCloudsSvg';
 import LandingGraphicLeft from './LandingGraphicLeft';
 import LandingGraphicRight from './LandingGraphicRight';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './LandingPage.css'
 
 function LandingPage() {
@@ -19,6 +19,15 @@ function LandingPage() {
                 </h1>
                 <p className="landing-sub-header">...talk with others around the world, and hang out with the people you love!</p>
                 <NavLink className="landing-action-button" to="/dashboard">Open Discourse</NavLink>
+            </div>
+            <div className="about-links-container">
+                <p className="about-links-dev-name">Created By Jason Cahela</p>
+                <a href="https://github.com/jcahela" target="_blank" rel="noreferrer">
+                    <img className="about-links-image" src="https://cdn.discordapp.com/attachments/886336420552269847/889198252417761290/GitHub-Mark-Light-32px.png" alt="" />
+                </a>
+                <a href="https://www.linkedin.com/in/jason-cahela/" target="_blank" rel="noreferrer">
+                    <img className="about-links-image" src="https://cdn.discordapp.com/attachments/886336420552269847/888542913594806272/768px-LinkedIn_logo_initials.png" alt="" />
+                </a>
             </div>
         </>
      );
