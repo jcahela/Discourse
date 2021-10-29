3a9997d29f83349ecf9d08ed58f27a8d31416f93
jasonc <jpacahela@gmail.com>
2021-10-29 18:30:32 -0500

    added mouse-down styling to logout button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router";
 import { useSelector } from "react-redux";
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user, socket }) {
+  const logoutRef = useRef();
+  const logoutTextRef = useRef();
   const sessionUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
   const history = useHistory();
@@ -34,12 +36,17 @@ function ProfileButton({ user, socket }) {
     socket.emit('set-offline', sessionUser.id)
   };
 
+  const setLogoutColor = () => {
+    logoutRef.current.style.backgroundColor = '#C93A3C'
+    logoutTextRef.current.style.color = 'white'
+  }
+
   return (
     <>
       <span onClick={openMenu} className="material-icons session-user-settings-icon">settings</span>
       {showMenu && (
-        <div className="profile-dropdown" onClick={logout}>
-            <span className="logout-button">Log Out</span>
+        <div ref={logoutRef} onMouseDown={setLogoutColor} className="profile-dropdown" onClick={logout}>
+            <span ref={logoutTextRef} className="logout-button">Log Out</span>
         </div>
       )}
     </>
