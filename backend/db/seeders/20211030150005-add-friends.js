3f580ec706846e79163130fa74aa982dbcf00103
jasonc <jpacahela@gmail.com>
2021-10-30 13:04:25 -0500

    added more friends seeders to both demo users

--- backend/db/seeders/20211030150005-add-friends.js
+++ backend/db/seeders/20211030150005-add-friends.js
@@ -15,6 +15,46 @@ module.exports = {
         user1: 1,
         user2: 4
       },
+      {
+        user1: 1,
+        user2: 5
+      },
+      {
+        user1: 1,
+        user2: 6
+      },
+      {
+        user1: 1,
+        user2: 7
+      },
+      {
+        user1: 1,
+        user2: 8
+      },
+      {
+        user1: 2,
+        user2: 7
+      },
+      {
+        user1: 2,
+        user2: 8
+      },
+      {
+        user1: 2,
+        user2: 9
+      },
+      {
+        user1: 2,
+        user2: 10
+      },
+      {
+        user1: 2,
+        user2: 11
+      },
+      {
+        user1: 2,
+        user2: 12
+      },
     ], {});
   },
 
