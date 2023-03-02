43b4ba06b31fd05dcdc96f33aa9e416851edbadc
jcahela <jpacahela@gmail.com>
2023-03-02 16:01:02 -0600

    Add cors allow frontend url as origin

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
 // Security Middleware
-if (!isProduction) {
-  // enable cors only in development
-  app.use(cors());
+if (isProduction) {
+  app.use(cors({ origin: 'https://discourse-aa.onrender.com' }));
 }
 // helmet helps set a variety of headers to better secure your app
 app.use(helmet({
