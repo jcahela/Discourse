aea0b80d02057a2e392ba691973e606ec786a1f5
jcahela <jpacahela@gmail.com>
2023-03-02 13:30:19 -0600

    Test values

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -2,8 +2,10 @@ const config = require('./index');
 const Sequelize = require('sequelize');
 
 console.log(process.env.DATABASE_URL);
+console.log(config);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || config.db.url, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: config.environment === 'development' ? 'localhost' : 'dpg-cfvse3t269v0ptnouacg-a',
   dialect: 'postgres',
   dialectOptions: {
     ssl: config.environment === 'development' ? false : true, // Required for connecting to Render's managed PostgreSQL service
